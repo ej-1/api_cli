d0af0c5a0897952b357c7a5aaf9c26a94a020b94
Erik <erikwjonsson@gmail.com>
2018-08-03 17:44:57 +0200

    Cleaned up ApiClient and added timeout handling from Faraday.

--- lib/api_client.rb
+++ lib/api_client.rb
@@ -1,61 +1,29 @@
 require 'json'
-require 'open-uri'
 require 'faraday'
 
 class ApiClient
 
+  # BASE_URL could be stored in a separate file and passed in to make the class even more agnostic.
   BASE_URI = 'https://driftrock-dev-test.herokuapp.com'.freeze
 
   def self.get(path) # BASE_URI
-    # stringyfiy data here
-      conn = Faraday.new(:url => BASE_URI)
-      response = conn.get path
-      data = JSON.parse(response.body)
+    # stringyfiy data here, but we don't really need that for this challenge.
+    conn = Faraday.new(:url => BASE_URI)
+    response = conn.get do |req|
+      req.url path
+      req.options.timeout = 5           # open/read timeout in seconds
+      req.options.open_timeout = 2      # connection open timeout in seconds
+    end
+    self.handle_response(response)
   end
 
-=begin
-  def get(twitter_user_name, count)
-    handle_timeouts do
-      response = access_token.request(:get, url(twitter_user_name, count))
-      handle_response(response)
-    end
-  end
-=end
   private
 
   def self.handle_response(response)
-    if response.code == '200'
-      if response.empty?
-        'ERROR: No tweets found for this user name!'
-      else
-        response
-      end
+    if response.status == 200
+      [JSON.parse(response.body)]
     else
-      JSON.parse(response.body)
-    end
-  end
-
-  def self.handle_timeouts_old
-    begin
-      yield
-    rescue Net::OpenTimeout, Net::ReadTimeout
-      { error: { message: 'Timeout error' } }
-    end
-  end
-
-  def self.handle_timeouts
-    perform_external_call
-  rescue Timeout::Error => e
-    @error = e
-    render :action => "error" # ??????? { error: { message: 'Timeout error' } }
-  end
-
-  def self.send_request(api_client, request_type, payload, count)
-    begin
-      api_client.send(request_type, payload, count)
-    rescue
-      # Retry request 1 time if request fails.
-      1.times { sleep 1, api_client.send(request_type, payload, count) }
+      [nil, JSON.parse(response)]
     end
   end
 end
