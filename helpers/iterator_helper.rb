72522f22fa0c732470a6924b4963cad984a84735
Erik <erikwjonsson@gmail.com>
2018-08-03 03:46:20 +0200

    Added tests. Create IteratorHelper class.

--- helpers/iterator_helper.rb
+++ helpers/iterator_helper.rb
@@ -1,24 +1,22 @@
 module IteratorHelper
-  def highest_count(array, key) # [{"user_id"=>"FFWN-1CKR-X4WU-Q44M", "item"=>"Awesome Marble Clock", "spend"=>"69.44"}, {"user_id"=>"HEI7-W5NW-OO9S-Z382", "item"=>"Synergistic Concrete Pants", "spend"=>"9.87"}, {"user_id"=>"HEI7-W5NW-OO9S-Z382", "item"=>"Synergistic Concrete Pants", "spend"=>"76.06"}, {"user_id"=>"BMCS-1VS1-39KR-7OUM", "item"=>"Durable Wool Shoes", "spend"=  =>  
-    item_names      = map_hash_value(array, key)
-    items_and_count = count_values(item_names)
-    items_and_count.max_by{ |k, v| v }.first
+  def highest_count(array, key)
+    values               = map_hash_value(array, key)
+    occurences_of_values = count_occurences(values)
+    occurences_of_values.max_by{ |k, v| v }.first
   end
 
   def map_hash_value(array, key)
     array.map { |h| h[key] }
   end
 
-  # [{:user=>"erik", :pants=>1}, {:user=>"fritz", :pants=>0}]  =>  {:user=>"erik", :pants=>1}
-  # user = match_hashes(response_users, 'email', user_email)
   def select_hashes(array, key, searched_value)
     array.select { |h| h[key] == searched_value }
   end
 
-  def count_values(keys)
+  def count_occurences(array)
     count = Hash.new(0)
-    keys.each do |key|
-      count[key] += 1
+    array.each do |element|
+      count[element] += 1
     end
     return count
   end
