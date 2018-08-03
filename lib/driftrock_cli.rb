9a2776ea96139a528e3db9cbcf6b02f51413e784
Erik <erikwjonsson@gmail.com>
2018-08-03 03:45:52 +0200

    Moved driftrock_cli to separate file.

---
+++ lib/driftrock_cli.rb
@@ -0,0 +1,62 @@
+require './helpers/iterator_helper'
+
+class DriftrockCli
+  extend IteratorHelper
+
+  def self.execute(args)
+    case args[0]
+    when 'most_sold'
+      response  = ApiClient.get('/purchases')
+      result    = most_sold(response['data'])
+      log(result)
+    when 'total_spend' # ruby app.rb total_spend schimmel_quincy@ernser.io 
+      user_email = args[1]
+      if user_email.empty?
+        log('Need a user email.')
+      else
+        response_users      = ApiClient.get('/users')
+        response_purchases  = ApiClient.get('/purchases')
+        result = total_spend(response_users['data'], response_purchases['data'])
+        log(result)
+      end
+    when 'most_loyal'
+      response            = ApiClient.get('/purchases')
+      response_users      = ApiClient.get('/users')
+      result = most_loyal(response_users['data'], response_purchases['data'])
+      log(result)
+    else
+      puts 'NEED ARGUMENTS'
+    end
+  end
+
+  def self.log(input)
+    puts input
+  end
+
+  def self.most_sold(users)
+    highest_count(users, 'item')
+  end
+
+  def self.total_spend(response_users, response_purchases, user_email)
+    user = select_hashes(response_users, 'email', user_email)
+    if user.empty?
+      # should the output maybe be JSON?
+      'No user found with that email.'
+    else
+      purchases = select_hashes(response_purchases, 'user_id', user.first['id'])
+      spendings = map_hash_value(purchases, 'spend')
+      if spendings.empty?
+        0.0
+      else
+        spendings.map(&:to_f).sum
+      end
+    end
+  end
+
+  def self.most_loyal(response_users, response_purchases)
+    user_id_with_most_purchases = highest_count(response_purchases, 'user_id')
+     # Would have been nice to have a API endpoint where you can get a user by id. GET /users/:id
+    user_with_most_purchases = response_users.find { |user| user['id'] == user_id_with_most_purchases }
+    user_with_most_purchases['email']
+  end
+end
